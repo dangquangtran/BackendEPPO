fa78d52e50a7a0a3eece5d391921078f1cc21968
dangquangtran <quangtran2002qn@gmail.com>
2024-11-19 19:52:01 +0700

    fix plant

--- DTOs/Plant/CreatePlantDTO.cs
+++ DTOs/Plant/CreatePlantDTO.cs
@@ -15,12 +15,10 @@ namespace DTOs.Plant
         public double? Width { get; set; }
         public double? Height { get; set; }
         public string Title { get; set; }
-        public double Price { get; set; }
-        public double Discounts { get; set; }
         public double FinalPrice { get; set; }
         public int? CategoryId { get; set; }
         public int? TypeEcommerceId { get; set; }
-        public IFormFile MainImageFile { get; set; }
+        public IFormFile? MainImageFile { get; set; }
         public List<IFormFile>? ImageFiles { get; set; }
     }
 }
--- DTOs/Plant/UpdatePlantDTO.cs
+++ DTOs/Plant/UpdatePlantDTO.cs
@@ -19,12 +19,11 @@ namespace DTOs.Plant
         public double Price { get; set; }
         public double Discounts { get; set; }
         public double FinalPrice { get; set; }
-        public string MainImage { get; set; }
         public int? CategoryId { get; set; }
         public int? TypeEcommerceId { get; set; }
         public int? Status { get; set; }
         public bool? IsActive { get; set; }
-        public IFormFile MainImageFile { get; set; }
+        public IFormFile? MainImageFile { get; set; }
         public List<IFormFile>? ImageFiles { get; set; }
     }
 }
